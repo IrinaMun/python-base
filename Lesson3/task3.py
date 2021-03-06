91a258240516049354647dc4f31d28e6d0d03f62
Irina <chibi85moon@gmail.com>
2021-03-06 18:49:40 +0300

    Решение 5-й задачи третьего урока
    Курс: Основы языка Python
    Урок 3. Функции. Словари

--- Lesson3/task3.py
+++ Lesson3/task3.py
@@ -10,4 +10,34 @@
 }
 
 Подумайте: полезен ли будет вам оператор распаковки? Сможете ли вы вернуть отсортированный по ключам словарь?
+
+делаем список с именами
+делаем пустой словарь
+выгрызаем первую букву имен, делаем ее ключом
+сравниваем первую букву имени и ключ, если идентичны, имя запихиваем в значение
 """
+
+# for el in employees:
+#     letter = el[0]
+#     tmp_list = []
+#     dict_employees[letter] = tmp_list
+#     if letter == el[0]:
+#         tmp_list.append(el)
+#         print(tmp_list)
+
+# print(dict_employees)
+
+employees = ['Игорь', 'Виктор', 'Илья', 'Вольдемар', 'Никита', 'Ирина', 'Наталья', 'Владимир', 'Нина']
+
+
+def thesaurus(*args):
+    dict_employees = {}
+    for el in args:
+        key = el[0]
+        if key not in dict_employees.keys():
+            dict_employees[key] = []
+        dict_employees[key].append(el)
+    return dict_employees
+
+
+print(thesaurus(*employees))
