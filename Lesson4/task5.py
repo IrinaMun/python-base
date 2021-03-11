57d00f11f663be58d039a129ef833d6e66f6cf3b
Irina <chibi85moon@gmail.com>
2021-03-11 10:20:44 +0300

    Задача 5
    Курс: Основы языка Python
    Урок 4. Работа с модулями и пакетами

--- Lesson4/task5.py
+++ Lesson4/task5.py
@@ -9,3 +9,33 @@ python task5.py USD EUR
 USD 75.18, 2020-09-05
 EUR 80.35, 2020-09-05
 """
+from sys import argv
+from requests import get
+import xml.etree.ElementTree as ElT
+from decimal import Decimal
+import datetime
+
+
+def get_currency_rate(currency):
+    """
+    Функция, возвращающая дату и курс заданной валюты по отношению к рублю на текущий момент.
+
+    :param currency: код валюты
+    :return: tuple из кода валюты, курса валюты по отношению к рублю (Decimal) и текущей даты
+    """
+    currency = currency.upper()
+    request = get('http://www.cbr.ru/scripts/XML_daily.asp')
+    root = ElT.fromstring(request.text)
+    date_obj = datetime.datetime.strptime(root.attrib['Date'], '%d.%m.%Y').date()
+    for el in root:
+        char_code = el.find('CharCode').text
+        if char_code == currency:
+            course = el.find('Value').text
+            course = Decimal(course.replace(",", "."))
+            return currency, course, f'{date_obj}'
+
+
+# for el in argv[1:]:
+#     print(get_currency_rate(el))
+
+[print(get_currency_rate(el)) for el in argv[1:]] # компрехенция
